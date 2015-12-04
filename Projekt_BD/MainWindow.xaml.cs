e7e34999de4f8d811887fa23dba4a6990951b52c
Yodde <szymon.klebowski@student.put.edu.pl>
2015-12-04 13:45:35 +0100

    Dodanie BacgroundWorkera
    Ale z bazą nadal się u mnie nie łączy.

--- Projekt_BD/MainWindow.xaml.cs
+++ Projekt_BD/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.Entity;
 using Projekt_BD.Migrations;
+using System.ComponentModel;
+using System.Windows.Threading;
 
 namespace Projekt_BD
 {
@@ -22,30 +24,44 @@ namespace Projekt_BD
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly BackgroundWorker worker = new BackgroundWorker();
         public MainWindow()
         {
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
             InitializeComponent();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e) {
+            StatusTworzeniaBazy.Dispatcher.Invoke(DispatcherPriority.Normal,
+                new Action(() => StatusTworzeniaBazy.Content = "Czekaj..."));
+            PierwszaFunkcja();
+        }
+        private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            StatusTworzeniaBazy.Dispatcher.Invoke(DispatcherPriority.Normal,
+                new Action(() => StatusTworzeniaBazy.Content = "Zakończono"));
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            worker.RunWorkerAsync();
+        }
+        private void PierwszaFunkcja() {
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<DbContext, Configuration>());
 
-            using (var db = new DbContext())
-            {
+            using(var db = new DbContext()) {
                 db.Database.Delete();
-
                 Guid id = Guid.NewGuid();
 
-                var jan = new Pacjent{ IdPacjenta = id, Imie= "Jan", DataUrodzenie = new DateTime(1994,1,6),Mail = "rower@op.pl"};
+                var jan = new Pacjent { IdPacjenta = id, Imie = "Jan", DataUrodzenie = new DateTime(1994, 1, 6), Mail = "rower@op.pl" };
 
                 db.Pacjentci.Add(jan);
 
-                var kardio = new Specjalizacja {IdSpecjalizacji = Guid.NewGuid(), Nazwa = "Kardiologia"};
+                var kardio = new Specjalizacja { IdSpecjalizacji = Guid.NewGuid(), Nazwa = "Kardiologia" };
 
                 db.Specjalizacje.Add(kardio);
 
-                var drMarcin = new Lekarz{IdLekarza = Guid.NewGuid(),Adres = "Kwiatowa 12",Imie = "Marcin", Nazwisko = "Nowak",Specjalizacja = kardio};
+                var drMarcin = new Lekarz { IdLekarza = Guid.NewGuid(), Adres = "Kwiatowa 12", Imie = "Marcin", Nazwisko = "Nowak", Specjalizacja = kardio };
 
                 db.Lekarze.Add(drMarcin);
 
@@ -53,7 +69,6 @@ namespace Projekt_BD
 
                 int i = db.SaveChanges();
             }
-
         }
     }
 }
