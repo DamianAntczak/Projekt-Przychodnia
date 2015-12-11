f0ddb14134b0d5441836dfe6d05f533a86431bf9
bartek94ka <bartosz.kaczorowski@student.put.poznan.pl>
2015-12-11 20:40:06 +0100

    Dodanie głównego widoku do obsługi przychodni
    Na razie przygotowałem tylko część do przeglądania bazy

---
+++ Projekt_BD/Views/MainPanel.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projekt_BD.Views
+{
+    /// <summary>
+    /// Interaction logic for MainPanel.xaml
+    /// </summary>
+    public partial class MainPanel : Window
+    {
+        public MainPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void PrzegladajBazeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItemName.Content = PrzegladajBazeButton.Content;
+            CenterPanel1.Visibility = Visibility.Visible;
+        }
+        private void ObsluzWizyteButton_Click(object sender, RoutedEventArgs e)
+        {
+            CenterPanel1.Visibility = Visibility.Hidden;
+        }
+        private void ZarzadzajWizytamiButton_Click(object sender, RoutedEventArgs e)
+        {
+            CenterPanel1.Visibility = Visibility.Hidden;
+        }
+    }
+}
