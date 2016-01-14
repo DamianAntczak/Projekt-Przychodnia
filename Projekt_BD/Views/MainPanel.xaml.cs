ccc2b6a06e043d2ff8970f3198696cbbff0de499
MrSmooku <d_antczak@op.pl>
2016-01-14 19:21:28 +0100

    Sprawdzanie czy pobierana wartość z grida nie jest null

--- Projekt_BD/Views/MainPanel.xaml.cs
+++ Projekt_BD/Views/MainPanel.xaml.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data.Entity.Core.Common.CommandTrees;
 using System.Data.Linq.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -115,37 +116,49 @@ namespace Projekt_BD.Views {
             var objekt = dataGrid_Wizyty.SelectedItem;
             //zabezpieczeniem przed wybraniem ostatniego rekordu (nie można rzutować na typ Wizyta)
             var typ = objekt.GetType();
+            if (typ.GetProperty("Data").GetValue(objekt) != null)
             Data.Text = typ.GetProperty("Data").GetValue(objekt).ToString();
-            CzasWizyty.Text = typ.GetProperty("CzasWizyty").GetValue(objekt).ToString();
-            tLekarz.Text = typ.GetProperty("Lekarz").GetValue(objekt).ToString();
-            tPacjenta.Text = typ.GetProperty("Pacjent").GetValue(objekt).ToString();
+            if (typ.GetProperty("CzasWizyty").GetValue(objekt) != null)
+                CzasWizyty.Text = typ.GetProperty("CzasWizyty").GetValue(objekt).ToString();
+            if (typ.GetProperty("Lekarz").GetValue(objekt) != null)
+                tLekarz.Text = typ.GetProperty("Lekarz").GetValue(objekt).ToString();
+            if (typ.GetProperty("Pacjent").GetValue(objekt) != null)
+                tPacjenta.Text = typ.GetProperty("Pacjent").GetValue(objekt).ToString();
         }
 
         private void dataGrid_Pacienci_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             var objekt = dataGrid_Pacienci.SelectedItem;
             var typ = objekt.GetType();
-            tImie.Text = typ.GetProperty("Imie").GetValue(objekt).ToString();
-            tNazwisko.Text = typ.GetProperty("Nazwisko").GetValue(objekt).ToString();
-            PESEL.Text = typ.GetProperty("Pesel").GetValue(objekt).ToString();
-            DataUrodzenia.Text = typ.GetProperty("Year").GetValue(objekt).ToString();
-            MiejsceUrodzenia.Text = typ.GetProperty("MiejsceUrodzenia").GetValue(objekt).ToString();
-            
+            if (typ.GetProperty("Imie").GetValue(objekt) != null)
+                tImie.Text = typ.GetProperty("Imie").GetValue(objekt).ToString();
+            if (typ.GetProperty("Nazwisko").GetValue(objekt) != null)
+                tNazwisko.Text = typ.GetProperty("Nazwisko").GetValue(objekt).ToString();
+            if (typ.GetProperty("Pesel").GetValue(objekt) != null)
+                PESEL.Text = typ.GetProperty("Pesel").GetValue(objekt).ToString();
+            if (typ.GetProperty("Year").GetValue(objekt) != null)
+                DataUrodzenia.Text = typ.GetProperty("Year").GetValue(objekt) != null ? typ.GetProperty("Year").GetValue(objekt).ToString() : " ";
+            if(typ.GetProperty("MiejsceUrodzenia").GetValue(objekt) != null)
+                MiejsceUrodzenia.Text = typ.GetProperty("MiejsceUrodzenia").GetValue(objekt).ToString();
         }
 
         private void dataGrid_Choroby_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var objekt = dataGrid_Choroby.SelectedItem;
             var typ = objekt.GetType();
-            NazwaChoroby.Text = typ.GetProperty("NazwaChoroby").GetValue(objekt).ToString();
-            OpisChoroby.Text = typ.GetProperty("Opis").GetValue(objekt).ToString();
-            Objawy.Text = typ.GetProperty("Objawy").GetValue(objekt).ToString();
+            if (typ.GetProperty("NazwaChoroby").GetValue(objekt) != null)
+                NazwaChoroby.Text = typ.GetProperty("NazwaChoroby").GetValue(objekt).ToString();
+            if (typ.GetProperty("Opis").GetValue(objekt) != null)
+                OpisChoroby.Text = typ.GetProperty("Opis").GetValue(objekt).ToString();
+            if (typ.GetProperty("Objawy").GetValue(objekt) != null)
+                Objawy.Text = typ.GetProperty("Objawy").GetValue(objekt).ToString();
         }
 
         private void dataGrid_Leki_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var objekt = dataGrid_Leki.SelectedItem;
             var typ = objekt.GetType();
-            NazwaLeku.Text = typ.GetProperty("NazwaLeku").GetValue(objekt).ToString();
+            if (typ.GetProperty("NazwaLeku").GetValue(objekt) != null)
+                NazwaLeku.Text = typ.GetProperty("NazwaLeku").GetValue(objekt).ToString();
         }
     }
 }
